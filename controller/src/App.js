3b43fa16a5850996b4ab6946299cf10f17df243d
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2018-04-24 22:13:59 +0200

    more styling

--- controller/src/App.js
+++ controller/src/App.js
@@ -2,25 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
+import { withStyles } from 'material-ui/styles';
+import Typography from "material-ui/es/Typography/Typography";
 
+const buttonOverrides = {
+  button: {
+    width: '150px',
+    padding: '30px',
+    textTransform: 'none',
+    fontSize: '120%',
+  },
+  heading: {
+    color: 'white',
+    textWeight: 'bold',
+  }
+};
+
+const ButtonSection = (props) =>
+  (<Grid className='main-grid' container spacing={24} justify='center' alignItems='center' >
+    <div >
+      <Typography className={props.classes.heading} variant='display2' align='center' >
+        <span >Hold the button to move </span >
+      </Typography >
+      <div className='center-panel' >
+        <section className='left-section' >
+          <Button className={props.classes.button} variant="raised" color="primary" >
+            <span >Pull up</span >
+          </Button >
+        </section >
+        <section className='right-section' >
+          <Button className={props.classes.button} variant="raised" color="secondary" >
+            <span >Push down</span >
+          </Button >
+        </section >
+      </div >
+    </div >
+  </Grid >);
+
+const Content = withStyles(buttonOverrides)(ButtonSection);
 
 class App extends Component {
   render() {
     return (
-      <Grid className='main-grid' container spacing={24} justify='center' alignItems='center' >
-        <div className='center-panel' >
-          <section className='left-section' >
-            <Button className="ctrl-button" variant="raised" color="primary" >
-              Table Up
-            </Button >
-          </section >
-          <section className='right-section' >
-            <Button className="ctrl-button" variant="raised" color="secondary" >
-              Table Down
-            </Button >
-          </section >
-        </div >
-      </Grid >
+      <Typography >
+        <Content />
+      </Typography >
     );
   }
 }
