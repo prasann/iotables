294ccfdc88c5b4fcd714dd5e5e56c53eefdd0da0
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2018-04-24 20:10:01 +0200

    Add material UI

--- controller/src/App.js
+++ controller/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from 'material-ui/Button';
+import Grid from 'material-ui/Grid';
+
 
 class App extends Component {
   render() {
     return (
-      <div >
-        <h1>IOTables</h1>
-      </div >
+      <Grid className='main-grid' container spacing={24} justify='center' alignItems='center' >
+        <div className='center-panel' >
+          <section className='left-section' >
+            <Button className="ctrl-button" variant="raised" color="primary" >
+              Table Up
+            </Button >
+          </section >
+          <section className='right-section' >
+            <Button className="ctrl-button" variant="raised" color="secondary" >
+              Table Down
+            </Button >
+          </section >
+        </div >
+      </Grid >
     );
   }
 }
