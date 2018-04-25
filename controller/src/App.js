d9daaab07aea96cc1dd6e695835e41e545738f7b
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2018-04-25 09:54:12 +0200

    refactor mover

--- controller/src/App.js
+++ controller/src/App.js
@@ -1,52 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
-import Button from 'material-ui/Button';
-import Grid from 'material-ui/Grid';
-import { withStyles } from 'material-ui/styles';
-import Typography from "material-ui/Typography";
-
-const buttonOverrides = {
-  button: {
-    width: '150px',
-    padding: '30px',
-    textTransform: 'none',
-    fontSize: '120%',
-  },
-  heading: {
-    color: 'white',
-    textWeight: 'bold',
-  }
-};
-
-const ButtonSection = (props) =>
-  (<Grid className='main-grid' container spacing={24} justify='center' alignItems='center' >
-    <div >
-      <Typography className={props.classes.heading} variant='display2' align='center' >
-        <span >Hold the button to move </span >
-      </Typography >
-      <div className='center-panel' >
-        <section className='left-section' >
-          <Button className={props.classes.button} variant="raised" color="primary" >
-            <span >Pull up</span >
-          </Button >
-        </section >
-        <section className='right-section' >
-          <Button className={props.classes.button} variant="raised" color="secondary" >
-            <span >Push down</span >
-          </Button >
-        </section >
-      </div >
-    </div >
-  </Grid >);
-
-const Content = withStyles(buttonOverrides)(ButtonSection);
+import Mover from './Mover';
 
 class App extends Component {
   render() {
     return (
-      <Typography >
-        <Content />
-      </Typography >
+        <Mover />
     );
   }
 }
---
+++ controller/src/Mover.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Typography from "material-ui/Typography";
+import Grid from 'material-ui/Grid';
+import Button from 'material-ui/Button';
+import { withStyles } from 'material-ui/styles';
+import './Mover.css';
+
+const styleOverrides = {
+  button: {
+    width: '150px',
+    padding: '30px',
+    textTransform: 'none',
+    fontSize: '120%',
+  },
+  heading: {
+    color: 'white',
+    textWeight: 'bold',
+  }
+};
+
+const MoverSection = (props) =>
+  (<Grid className='main-grid' container spacing={24} justify='center' alignItems='center' >
+    <div >
+      <Typography className={props.classes.heading} variant='display2' align='center' >
+        <span >Hold the button to move </span >
+      </Typography >
+      <div className='center-panel' >
+        <section className='left-section' >
+          <Button className={props.classes.button} variant="raised" color="primary" >
+            <span >Up</span >
+          </Button >
+        </section >
+        <section className='right-section' >
+          <Button className={props.classes.button} variant="raised" color="secondary" >
+            <span >Down</span >
+          </Button >
+        </section >
+      </div >
+    </div >
+  </Grid >);
+
+export default withStyles(styleOverrides)(MoverSection);
